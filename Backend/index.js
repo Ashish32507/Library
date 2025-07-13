4a1485d95d3e148fa0bc3457a1393a35f2eefa08
Ashish Kumar Yadav <103557142+Ashish32507@users.noreply.github.com>
2025-07-13 08:56:21 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const issueRoutes = require("./routes/issueRoutes"); // Corrected variable
 app.use(express.json());
 app.use(cookieParser());
 const corsOption = {
-  origin: "*",
+  origin: "https://library-silk-mu.vercel.app/",
 };
 app.use(cors(corsOption)); // Enable CORS if needed
 
